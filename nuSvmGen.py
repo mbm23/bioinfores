9a96d0ba929b51ed0ab01c15d5ee48f56e53bd33
mbm23 <mbm23@aol.com>
2014-03-10 15:55:17 -0400

    Test files added

--- nuSvmGen.py
+++ nuSvmGen.py
@@ -1,39 +1,42 @@
 import random
+import unicodedata
 import sys 
 
-def simulate_sequence (length) :
-        dna = ['A','G','C','T']
-        sequence = ''
-        for i in range (length) :
-                sequence += random.choice (dna)
-        return sequence
+
+def simulate_sequence(length):
+     dna = ['a','g','c','t']
+     sequence = ''
+     for i in range (length) :
+          sequence += random.choice (dna)
+     return sequence
 
 def defineVars(f,numChar,lenDna):
-        f.write("MODULE main\n")
-        f.write("VAR\n")
-        for i in range(0,numChar):
-                f.write('a' + str(i) + ": {a, c, t, g};\n")
-        
-        f.write("VAR pos: 0.." + str(lenDna) + ";\n")
-        
+     f.write("MODULE main\n")
+     f.write("VAR\n")
+     for i in range(0,numChar):
+           f.write('a' + str(i) + ": {a, c, t, g};\n")
+     
+     f.write("VAR pos: 0.." + str(lenDna) + ";\n")
+      
 def writeProg(numChar, seq,f):
-        f.write("INIT (\n")
-        for i in range(0,len(seq)-numChar - 1):
-                f.write("(")
-                for j in range(0,numChar):
-                        f.write("a" + str(j) + " = " + seq[i+j] + " & ")
-                f.write("pos= " + str(i) +")|\n")
-        f.write("(")
-        for j in range(0,numChar):
-                f.write("a" + str(j) + " = " + seq[i+j] + " & ")
-        f.write("pos= " + str(i + 1) +") \n )")                
+     f.write("INIT (\n")
+     i =0 
+     for i in range(0,len(seq)-numChar - 1):
+           f.write("(")
+           for j in range(0,numChar):
+                 f.write("a" + str(j) + "=" + seq[i+j] + " & ")
+           f.write("pos=" + str(i) +")|\n")
+     f.write("(")
+     for j in range(0,numChar):
+           f.write("a" + str(j) + "=" + seq[i+j] + " & ")
+     f.write("pos=" + str(i + 1) +") \n )")                
         
         
         
 c =10
-l =500
+l =7010
 seq = simulate_sequence(l)
-fhand = open('test.smv','w')
+fhand = open('C:\\Users\\mcallimb\\Documents\\GitHub\\bioinfores\svnsample7000.smv','w')
 
 defineVars(fhand,c,l)
 writeProg(c, seq,fhand)
